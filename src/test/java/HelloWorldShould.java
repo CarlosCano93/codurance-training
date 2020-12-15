db057ab2a57c55bdec225b5c0e30da970fa799fe
carloscano93 <carlcan.93@gmail.com>
2020-12-15 09:53:37 +0100

    first commit

---
+++ src/test/java/HelloWorldShould.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class HelloWorldShould {
+
+    @Test
+    void name() {
+        assertThat(true).isEqualTo(true);
+
+    }
+}
