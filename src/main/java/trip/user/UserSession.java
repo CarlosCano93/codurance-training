e95d662e54bb6403af1bb6a1dcc9b73ea0aba119
carloscano93 <carlcan.93@gmail.com>
2020-12-17 09:11:39 +0100

    trip refactor kata

---
+++ src/main/java/trip/user/UserSession.java
@@ -0,0 +1,22 @@
+package trip.user;
+
+
+import trip.exception.CollaboratorCallException;
+
+public class UserSession {
+
+    private static final UserSession userSession = new UserSession();
+
+    private UserSession() {
+    }
+
+    public static UserSession getInstance() {
+        return userSession;
+    }
+
+    public User getLoggedUser() {
+        throw new CollaboratorCallException(
+                "UserSession.getLoggedUser() should not be called in an unit test");
+    }
+
+}
